ff9afdc728a81472b2a61c7f03e790e97806b2ae
afnaan-ahmed <afnaan2180@gmail.com>
2024-04-21 14:55:03 +0530

    commit

---
+++ script/script.js
@@ -0,0 +1,36 @@
+function Swiggy() {
+    //const url = "#"
+    //window.open(url)
+}
+function Zomato() {
+    //const url = "#"
+    //window.open(url)
+}
+
+
+function Menu() {
+    window.open("menu.html")
+}
+
+
+
+/* testimonial section code */
+
+let slideIndex = 0;
+showTestimonial();
+
+function showTestimonial() {
+    let testimonials = document.getElementsByClassName("testimonial");
+    for (let i = 0; i < testimonials.length; i++) {
+        testimonials[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > testimonials.length) {
+        slideIndex = 1;
+    }
+    testimonials[slideIndex - 1].style.display = "block";
+    setTimeout(showTestimonial, 3000); // Change testimonial every 6 seconds
+}
+function changeTestimonial(n) {
+    showTestimonial(slideIndex += n);
+}
